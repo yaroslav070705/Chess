3f89f5748c67254a9c605789084050b3e4042776
Anna <62518959+Anna-Kuzmenkova@users.noreply.github.com>
2021-03-13 00:48:54 +0300

    Update King.h

--- King.h
+++ King.h
@@ -1,23 +1,29 @@
-///< @author Roma,Timofey,Yaroslav
+///< @file King.h
 
+/**
+ @author Roma, Timofey, Yaroslav
 
-class King: public BaseFigure{   ///< класс короля
-    King(int type0, int x_cell0, int y_cell0, COLORREF main_color0, COLORREF side_color0) : BaseFigure(type0, x_cell0, y_cell0, main_color0, side_color0){}      ///< конструктор
-    virtual void draw(int type0, int x_cell0, int y_cell0) override;                                                          ///< рисование
-    virtual void count_move_ability_cells(vector<BaseFigure*> figures) override;               ///< просчёт возможности ходов
+ King - класс фигуры короля;
+
+ Потомок класса BaseFigure.
+*/
+class King:public BaseFigure{
+public:
+    /// конструктор
+    King(int type0, int x_cell0, int y_cell0, COLORREF main_color0, COLORREF side_color0) : BaseFigure(type0, x_cell0, y_cell0, main_color0, side_color0){}
+    virtual void draw(int type0, int x_cell0, int y_cell0) override;                    // рисование
+    virtual void count_move_ability_cells(vector<BaseFigure*> figures) override;        // просчёт возможности ходов
     virtual void change() override{}
-    /*void printArray(){
-      cout<<"size="<<move_ability_cells.size()<<'\n';
-      for (int i = 0; i < move_ability_cells.size(); i++){
-         cout<<"[y"<<i<<"]="<<move_ability_cells[i][0]<<"  ";
-         cout<<"[x"<<i<<"]="<<move_ability_cells[i][1]<<'\n';
-      }
-    } */
 };
-
+/**
+ Функция рисования короля;
+ \param x  - координата короля по оси x;
+ \param y  - координата короля по оси y;
+ \param r  - размер короля.
+ */
 void King::draw(int x, int y, int r){
     txSetColor(side_color,2);
-    txSetFillColor (RGB (255, 215, 0)); //золото для короны
+    txSetFillColor (RGB (255, 215, 0)); // золото для короны
 
     POINT headtop[25] = {{(6*r)+x, (0*r)+y}, {(7*r)+x, (6*r)+y}, {(8*r)+x, (0*r)+y},
                      {(9*r)+x, (6*r)+y}, {(10*r)+x, (0*r)+y}, {(11*r)+x, (6*r)+y},
@@ -40,26 +46,33 @@ void King::draw(int x, int y, int r){
                            {(4*r)+x, (5*r)+y}, {(2*r)+x, (5*r)+y}}; //"голова", меняется в зависимости от фигуры
     txPolygon (headnottop, 26);*/
 
-    txSetFillColor (main_color); //основной цвет (белый)
-    //txSetFillColor (RGB (255, 255, 255)); //основной цвет (чёрный)
+    txSetFillColor (main_color); // основной цвет (белый)
+    //txSetFillColor (RGB (255, 255, 255)); ///< основной цвет (чёрный)
 
-    POINT body[4] = {{(10*r)+x, (20*r)+y}, {(12*r)+x, (20*r)+y}, {(16*r)+x, (40*r)+y}, {(6*r)+x, (40*r)+y}}; //неизменяемое тело
+    POINT body[4] = {{(10*r)+x, (20*r)+y}, {(12*r)+x, (20*r)+y}, {(16*r)+x, (40*r)+y}, {(6*r)+x, (40*r)+y}}; // неизменяемое тело
     txPolygon (body, 4);
 
-    POINT stand1[4] = {{(4*r)+x, (40*r)+y}, {(18*r)+x, (40*r)+y}, {(18*r)+x, (42*r)+y}, {(4*r)+x, (42*r)+y}}; //первая подставка для всех, а у пешек только она
+    POINT stand1[4] = {{(4*r)+x, (40*r)+y}, {(18*r)+x, (40*r)+y}, {(18*r)+x, (42*r)+y}, {(4*r)+x, (42*r)+y}}; // первая подставка для всех, а у пешек только она
     txPolygon (stand1, 4);
 
-    POINT stand2[4] = {{(2*r)+x, (42*r)+y}, {(20*r)+x, (42*r)+y}, {(20*r)+x, (44*r)+y}, {(2*r)+x, (44*r)+y}}; //вторая подставка для средних(ладья,слон,конь) и лучших
+    POINT stand2[4] = {{(2*r)+x, (42*r)+y}, {(20*r)+x, (42*r)+y}, {(20*r)+x, (44*r)+y}, {(2*r)+x, (44*r)+y}}; // вторая подставка для средних(ладья,слон,конь) и лучших
     txPolygon (stand2, 4);
 
-    POINT stand3[4] = {{(0*r)+x, (44*r)+y}, {(22*r)+x, (44*r)+y}, {(22*r)+x, (46*r)+y}, {(0*r)+x, (46*r)+y}};//третья подставка для лучших(ферзей, королей)
+    POINT stand3[4] = {{(0*r)+x, (44*r)+y}, {(22*r)+x, (44*r)+y}, {(22*r)+x, (46*r)+y}, {(0*r)+x, (46*r)+y}};// третья подставка для лучших(ферзей, королей)
     txPolygon (stand3, 4);
 }
+/**
+ Функция просчёта возможности хода;
 
+ \param figures  - вектор фигур.
+
+ Король ходит и ест во все стороны на одну клетку;
+
+*/
 void King :: count_move_ability_cells(vector<BaseFigure*> figures) {
-    bool out = false;
+    move_ability_cells.clear();
     if((x_cell+1) < 8 && (y_cell-1) >= 0)
-    {                                           ///< вверх право
+    {                                           // вверх право
         int* cell = new int[2];
         cell[0] = y_cell - 1;
         cell[1] = x_cell + 1;
@@ -69,13 +82,13 @@ void King :: count_move_ability_cells(vector<BaseFigure*> figures) {
                    if(type == figura->get_type()){
                         move_ability_cells.pop_back();
                    }
-                   out = true;
+                  // out = true;
                    break;
             }
         }
     }
     if((x_cell -1) >= 0 && (y_cell-1) >= 0)
-    {                                           ///< вверх влево
+    {                                           // вверх влево
         int* cell = new int[2];
         cell[0] = y_cell - 1;
         cell[1] = x_cell - 1;
@@ -85,13 +98,13 @@ void King :: count_move_ability_cells(vector<BaseFigure*> figures) {
                    if(type == figura->get_type()){
                         move_ability_cells.pop_back();
                    }
-                   out = true;
+                 //  out = true;
                    break;
             }
         }
     }
     if((x_cell+1) < 8 && (y_cell+1) < 8)
-    {                                           ///< вниз вправо
+    {                                           // вниз вправо
         int* cell = new int[2];
         cell[0] = y_cell + 1;
         cell[1] = x_cell + 1;
@@ -101,13 +114,13 @@ void King :: count_move_ability_cells(vector<BaseFigure*> figures) {
                    if(type == figura->get_type()){
                         move_ability_cells.pop_back();
                    }
-                   out = true;
+                 //  out = true;
                    break;
             }
         }
     }
     if((x_cell-1) >= 0 && (y_cell+1) < 8)
-    {                                           ///< вниз влево
+    {                                           // вниз влево
         int* cell = new int[2];
         cell[0] = y_cell + 1;
         cell[1] = x_cell - 1;
@@ -117,13 +130,13 @@ void King :: count_move_ability_cells(vector<BaseFigure*> figures) {
                    if(type == figura->get_type()){
                         move_ability_cells.pop_back();
                    }
-                   out = true;
+                  // out = true;
                    break;
             }
         }
     }
     if((y_cell-1) >= 0)
-    {                                           ///< вверх
+    {                                           // вверх
         int* cell = new int[2];
         cell[0] = y_cell - 1;
         cell[1] = x_cell;
@@ -133,13 +146,13 @@ void King :: count_move_ability_cells(vector<BaseFigure*> figures) {
                    if(type == figura->get_type()){
                         move_ability_cells.pop_back();
                    }
-                   out = true;
+                   //out = true;
                    break;
             }
         }
     }
     if((y_cell+1) < 8)
-    {                                           ///< вниз
+    {                                           // вниз
         int* cell = new int[2];
         cell[0] = y_cell + 1;
         cell[1] = x_cell;
@@ -149,13 +162,13 @@ void King :: count_move_ability_cells(vector<BaseFigure*> figures) {
                    if(type == figura->get_type()){
                         move_ability_cells.pop_back();
                    }
-                   out = true;
+                   //out = true;
                    break;
             }
         }
     }
     if((x_cell+1) < 8)
-    {                                           ///< вправо
+    {                                           // вправо
         int* cell = new int[2];
         cell[0] = y_cell;
         cell[1] = x_cell + 1;
@@ -165,13 +178,13 @@ void King :: count_move_ability_cells(vector<BaseFigure*> figures) {
                    if(type == figura->get_type()){
                         move_ability_cells.pop_back();
                    }
-                   out = true;
+                   //out = true;
                    break;
             }
         }
     }
     if((x_cell-1) >= 0)
-    {                                           ///< влево
+    {                                           // влево
         int* cell = new int[2];
         cell[0] = y_cell;
         cell[1] = x_cell - 1;
@@ -181,7 +194,7 @@ void King :: count_move_ability_cells(vector<BaseFigure*> figures) {
                    if(type == figura->get_type()){
                         move_ability_cells.pop_back();
                    }
-                   out = true;
+                   //out = true;
                    break;
             }
         }
